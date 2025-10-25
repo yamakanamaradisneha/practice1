0c1cebcb55dbe89b96d1f7defa39776110448fe9
yamakanamaradisneha <yamakanamaradisneha@gmail.com>
2025-10-25 08:37:44 +0530

    Update math.py

--- math.py
+++ math.py
@@ -3,7 +3,7 @@ try:
    a = float(input("Enter first number:"))
    b = float(input("enter second number:"))
 
-   print(f"Addition:{a+b}")
-   print(f"Subtraction:{a-b}")
+   print(f"Addition: {a+b}")
+   print(f"Subtraction: {a-b}")
 except ValueError:
   print("Please enter valid numbers.")
