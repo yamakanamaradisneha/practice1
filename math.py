8bdc324108604906b62f756b2eb71f975e3e6920
yamakanamaradisneha <yamakanamaradisneha@gmail.com>
2025-10-25 08:29:48 +0530

    Create math.py

---
+++ math.py
@@ -0,0 +1,9 @@
+#math.py
+try:
+   a = float(input("Enter first number:"))
+   b = float(input("enter second number:"))
+
+   print(f"Addition:{a+b}")
+   print(f"Subtraction:{a-b}")
+except ValueError:
+  print("Please enter valid numbers.")
